647fbaa520ee27dbcc719dff301cc28b68c3259e
Geoffrey GREBERT <ggrebert@sii.fr>
2021-07-01 13:33:11 +0200

    add docker

--- kafka/java/order-camunda/src/main/java/io/flowing/retail/order/flow/SayHelloAdapter.java
+++ kafka/java/order-camunda/src/main/java/io/flowing/retail/order/flow/SayHelloAdapter.java
@@ -1,12 +1,12 @@
 package io.flowing.retail.order.flow;
 
-import io.flowing.retail.order.messages.Message;
-import io.flowing.retail.order.messages.MessageSender;
-import io.flowing.retail.order.persistence.OrderRepository;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import io.flowing.retail.order.messages.Message;
+import io.flowing.retail.order.messages.MessageSender;
+import io.flowing.retail.order.persistence.OrderRepository;
 
 @Component
 public class SayHelloAdapter implements JavaDelegate {
@@ -21,7 +21,7 @@ public class SayHelloAdapter implements JavaDelegate {
   public void execute(DelegateExecution context) throws Exception {
     long ordersNumber = orderRepository.count();
     String traceId = context.getProcessBusinessKey(); 
-    
+    System.err.println("\n\nHello ! 😊 💋 \n\n");
     messageSender.send( //
         new Message<SayHelloCommandPayload>( //
             "SayHelloCommand", //
